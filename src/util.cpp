8e3fd4dd3285bc06d841dea6aa032a4bbf823762
drkw <drkw.shop@gmail.com>
2018-02-04 12:52:42 +0900

    Bug fix : ZcashParams is not changed by BaseParams

--- src/util.cpp
+++ src/util.cpp
@@ -506,7 +506,7 @@ static boost::filesystem::path ZC_GetBaseParamsDir()
 #endif
 }
 
-const boost::filesystem::path &ZC_GetParamsDir(bool fNetSpecific)
+const boost::filesystem::path &ZC_GetParamsDir()
 {
     namespace fs = boost::filesystem;
 
